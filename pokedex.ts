9c2e4f43417b2c99e82f1eb33fde9b17386130f0
Stef De Bie <stef.debie@student.ap.be>
2025-02-17 10:51:49 +0000

    added styling for nav, main and footer

--- pokedex.ts
+++ pokedex.ts
@@ -3,10 +3,10 @@ async function displayPokemonOnCard() {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
         if (!response.ok) throw new Error(`Pokemon not found: ${response.statusText}`);
         const data = await response.json();
-        const imgElement = document.querySelector(".pokedex__card_img-front") as HTMLImageElement;
-        const nameElement = document.querySelector(".pokedex__card__name") as HTMLElement;
-        const idElement = document.querySelector(".pokedex__card__id") as HTMLElement;
-        const typesElement = document.querySelector(".pokedex__card__types") as HTMLElement;
+        const imgElement = document.querySelector(".pokedex__card__img--front") as HTMLImageElement;
+        const nameElement = document.querySelector(".pokedex__card__left__name") as HTMLElement;
+        const idElement = document.querySelector(".pokedex__card__left__id") as HTMLElement;
+        const typesElement = document.querySelector(".pokedex__card__left__types") as HTMLElement;
         nameElement.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
         idElement.textContent = `#${data.id}`;
         typesElement.textContent = `${data.types.map((t: any) => t.type.name).join(", ")}`;
