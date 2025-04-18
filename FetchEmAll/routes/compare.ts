b24c4502904527f351136d2462c90aeed1396eb3
Stef De Bie <ssstef.de.bie@gmail.com>
2025-04-18 12:29:03 +0000

    made most compare stats dynamic

--- FetchEmAll/routes/compare.ts
+++ FetchEmAll/routes/compare.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { getPokemonStats } from "./pokemon";
 import { Pokemon } from "./pokedex";
 
 const compareRoute = express.Router();
@@ -8,9 +9,12 @@ compareRoute.get("/", async (req, res) => {
     const id1:number = typeof req.query.id1 === "string" ? parseInt(req.query.id1) : 1;
     const id2:number = typeof req.query.id2 === "string" ? parseInt(req.query.id2) : 2;
 
+    const poke1 = await getPokemonStats(id1);
+    const poke2 = await getPokemonStats(id2);
+
     res.render("compare", {
-        id1,
-        id2
+        poke1,
+        poke2
     });
 });
 
