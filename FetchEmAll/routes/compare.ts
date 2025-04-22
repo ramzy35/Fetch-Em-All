d3d9e5bb8d3e361b69e251f58c5f52f49f17bae3
Stef De Bie <ssstef.de.bie@gmail.com>
2025-04-22 13:38:19 +0000

    type damage in compare and moved functions to seperate file

--- FetchEmAll/routes/compare.ts
+++ FetchEmAll/routes/compare.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPokemonStats } from "./pokemon";
+import { getPokemonStats, PokemonStats } from "../pokemonStats";
 import { Pokemon } from "./pokedex";
 
 const compareRoute = express.Router();
@@ -10,8 +10,8 @@ compareRoute.get("/", async (req, res) => {
     const id2:number = typeof req.query.id2 === "string" ? parseInt(req.query.id2) : 2;
     // standard values for both id's incase no id is given
 
-    const poke1:Pokemon = await getPokemonStats(id1);
-    const poke2:Pokemon = await getPokemonStats(id2);
+    const poke1:PokemonStats = await getPokemonStats(id1);
+    const poke2:PokemonStats = await getPokemonStats(id2);
     
     res.locals.currentPage = "compare" 
 
