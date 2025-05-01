7de4afeac6c13a2543b664841fcd34bc55631b1d
Stef De Bie <ssstef.de.bie@gmail.com>
2025-05-01 15:58:59 +0000

    fixing dumb error

---
+++ FetchEmAll/middleware/pokeNames.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction} from "express";
+import { getAllPokemon } from "../database";
+
+export async function getPokeNames(req:Request, res:Response, next:NextFunction){
+    const pokeList = await getAllPokemon()
+    res.locals.pokemonNameList = [];
+    // site crashes when this line is not here, looking into this later
+    pokeList.forEach(poke => {
+        res.locals.pokemonNameList.push(poke?.name) 
+    });
+    next();
+}
