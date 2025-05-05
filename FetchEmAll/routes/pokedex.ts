4fdc3157fac1125bdf8677dd730c88f2c5f4bdc8
dvdbdev <Dries.Van.Den.Brande@proton.me>
2025-05-05 17:55:50 +0000

    POST starter

--- FetchEmAll/routes/pokedex.ts
+++ FetchEmAll/routes/pokedex.ts
@@ -3,9 +3,16 @@ import { pokeListLocal } from "../middleware/locals";
 
 const pokedexRoute = express.Router();
 
+pokedexRoute.post("/choose-starter", (req, res) => {
+    const { starterId } = req.body;
+    console.log("Starter chosen:", starterId);
+    res.redirect("/pokedex?hasStarter=true");
+});
+
 pokedexRoute.get("/", pokeListLocal, async (req, res) => {
-    res.locals.currentPage = "pokedex" 
-    res.render("pokedex");
+    res.locals.currentPage = "pokedex";
+    const hasStarter = req.query.hasStarter === "true";
+    res.render("pokedex", { hasStarter });
 });
 
 export default pokedexRoute;
