be01bd9ce827cb6f90982dcac781f759ea941eb1
Stef De Bie <ssstef.de.bie@gmail.com>
2025-05-12 14:52:43 +0000

    added comments to locals.ts and tweaked getCompareStyling

--- FetchEmAll/middleware/locals.ts
+++ FetchEmAll/middleware/locals.ts
@@ -3,6 +3,7 @@ import { getMyPokemon, getAllPokemon, getPokemonById } from "../database";
 import { FullPokemon, MyPokemon, Pokemon } from "../interfaces";
 import { ObjectId } from "mongodb";
 
+// Array of plain text pokemon names
 export async function pokeNamesLocal(_req:Request, res:Response, next:NextFunction){
     const pokeList : Pokemon[]  = await getAllPokemon()
     res.locals.pokemonNameList = [];
@@ -13,18 +14,16 @@ export async function pokeNamesLocal(_req:Request, res:Response, next:NextFuncti
     next();
 }
 
+// Array of pokemon owned by user
 export async function myPokemonLocal(req:Request, res:Response, next:NextFunction){
-    if (req.session.user && typeof req.session.user != "undefined" && typeof req.session.user._id != "undefined") {
-        res.locals.user = req.session.user;
-    } else {
-        res.redirect("/login");
-    }
+    res.locals.user = req.session.user;
     const myPokemon : FullPokemon[] = await getMyPokemon(res.locals.user._id)
     res.locals.myPokemon = myPokemon;
 
     next();
 }
 
+// All owned pokemon ids and all pokemon
 export async function pokeListLocal(req: Request, res: Response, next: NextFunction) {
     if (req.session.user && typeof req.session.user != "undefined" && typeof req.session.user._id != "undefined") {
         const myPokemon : FullPokemon[] = await getMyPokemon(req.session.user._id)
@@ -39,14 +38,17 @@ export async function pokeListLocal(req: Request, res: Response, next: NextFunct
     next();
 }
 
+// Various formatting and styling functions
 export async function formattingLocals(_req: Request, res: Response, next: NextFunction) {
-    res.locals.getTypes = (types: string[]) : string=> {
+    res.locals.getTypes = (types: string[]) : string => {
         return types.join(", ");
     }
+
     res.locals.convert = (num: number) : number => {
         return num/10;
     }
-    res.locals.generation = (gen: string) => {
+
+    res.locals.generation = (gen: string) : string => {
         const map: Record<string, string> = {
             "generation-i": "I",
             "generation-ii": "II",
@@ -59,10 +61,12 @@ export async function formattingLocals(_req: Request, res: Response, next: NextF
         };
         return map[gen] || gen;
     };
+
     res.locals.formatId = (id : number) : string => {
         const idString : string = "000" + id.toString()
         return `#${idString.slice(-3)}`
     }
+
     res.locals.getPercentAndColor = (currentHp: number, maxHp: number): string => {
         const percent = currentHp / maxHp;
         let color: string;
@@ -74,25 +78,30 @@ export async function formattingLocals(_req: Request, res: Response, next: NextF
         } else {
             color = "#ff0000"; // Red
         }
-
         return `"width: calc(100% * ${percent}); background-color: ${color}"`;
     };
+
     res.locals.getPercent = (stat: number, statMax: number): string => {
         return `"width: calc(100% * ${stat} / ${statMax});"`;
     };
+
     res.locals.getCompareStyling = (stat1: number, stat2: number, statMax : number): string => {
         const percent = stat1 / statMax;
         let color:string;
-        if(stat1 >= stat2) {
+        if(stat1 > stat2) {
             color = "#008000";
+        } else if(stat1 === stat2) {
+            color = "#ffff00";
         } else {
             color = "#ff0000";
         }
         return `"width: calc(100% * ${percent}); background-color: ${color}"`;
     };
+
     next();
 }
 
+// Check if user has starter Pokemon
 export async function hasStarterLocal(req: Request, res: Response, next: NextFunction) {
     const ownerId = req.session.user?._id;
     if (ownerId) {
