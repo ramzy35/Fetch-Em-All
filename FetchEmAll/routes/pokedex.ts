6cb0a818a5100127e23aedc84eb053bb3f71b910
DVdBdev <dries.van.den.brande@proton.me>
2025-05-12 23:26:58 +0000

    nickname POST + queries

--- FetchEmAll/routes/pokedex.ts
+++ FetchEmAll/routes/pokedex.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { query } from "express";
 import { hasStarterLocal, pokeListLocal } from "../middleware/locals";
 import { catchPokemon, changeCurrentPokemon, deleteMyPokemon, getMyPokemon, getMyPokemonById, levelPokemon, renamePokemon } from "../database";
 import { secureMiddleware } from "../middleware/secureMiddleware";
@@ -9,17 +9,34 @@ const pokedexRoute = express.Router();
 
 pokedexRoute.post("/choose-starter", secureMiddleware, async (req, res) => {
     const { starterId } = req.body;
-    await deleteMyPokemon(res.locals.user._id);
-    await catchPokemon(parseInt(starterId), res.locals.user._id, 0);
-    await changeCurrentPokemon(parseInt(starterId), res.locals.user._id);
-    await levelPokemon(parseInt(starterId), res.locals.user._id);
+
+    const userId = res.locals.user._id;
+    const starterIdNum = parseInt(starterId);
+    await deleteMyPokemon(userId);
+    await catchPokemon(starterIdNum, userId, 0);
+    await changeCurrentPokemon(starterIdNum, userId);
+    await levelPokemon(starterIdNum, userId);
+
     console.log("Starter chosen:", starterId);
-    res.redirect("/pokedex?hasStarter=true");
+
+    res.redirect(`/pokedex?justChoseStarter=1&starterId=${starterIdNum}`);
 });
 
 pokedexRoute.get("/", pokeListLocal, secureMiddleware, hasStarterLocal, async (req, res) => {
     res.locals.currentPage = "pokedex";
-    res.render("pokedex");
+    res.render("pokedex", { hasStarter: res.locals.hasStarter, query: req.query });
+});
+
+pokedexRoute.post("/nickname",secureMiddleware, async (req, res) => {
+    const { nickname, starterId } = req.body;
+    const userId = res.locals.user._id;
+
+    if (starterId && nickname) {
+        await renamePokemon(parseInt(starterId, 10), userId, nickname);
+        console.log(`Nickname set: ${nickname} for Pokémon ID ${starterId}`);
+    }
+
+    res.redirect("/pokedex");
 });
 
 pokedexRoute.get("/:status", async (req, res) => {
